0c018de579cf28614261fecd8ddce511c027a935
anni-mao <67307378+anni-mao@users.noreply.github.com>
2021-10-27 22:37:29 -0700

    Initial Commit

---
+++ FaceTrackingApplication/FaceTrackingApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceTrackingApplicationApp.swift
+//  FaceTrackingApplication
+//
+//  Created by Anni Mao on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct FaceTrackingApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
